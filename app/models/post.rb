c1304548dc6e680c781a5f88896ab5c2dceefe47
Chely Ho <adept_inept@yahoo.com>
2018-04-09 21:01:38 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :ClickBaitTitle
+
+  def ClickBaitTitle
+    /\bWon't Believe|\bSecret|\bTop \d+|\bGuess/.match(self.title)
+  end
 end
