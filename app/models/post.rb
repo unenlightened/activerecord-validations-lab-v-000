fedff6ab21e3f8987d383df8399be05159757e24
Chely Ho <adept_inept@yahoo.com>
2018-04-09 21:08:10 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :ClickBaitTitle
+
+  def ClickBaitTitle
+    if !/\bWon't Believe|\bSecret|\bTop \d+|\bGuess/.match(self.title)
+      errors.add(:title, "is not click baitey")
+    end
+  end
 end
